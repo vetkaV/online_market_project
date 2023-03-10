ea612378a9ba3834568a35f3fae680f65ff530be
vetkaV <125943529+vetkaV@users.noreply.github.com>
2023-03-10 09:44:59 +0000

    add register page and reg function

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, session, request, redirect, url_for, render_template
 import os
-from startSqlite import get_succes_auth
+from startSqlite import get_succes_auth, create_user
 def get_auth():
     login = request.form.get('auth_login')
     pass_w = request.form.get('auth_pass')
@@ -15,17 +15,28 @@ def index():
 def auth():
     if request.method == "POST":
         if get_auth(): 
-            return redirect(url_for('main'))
+            return redirect(url_for('index'))
 
     return render_template('authorization.html')
 
 def catalog():
     return render_template('catalog.html')    
+
+def reg():
+    result = True
+    if request.method == "POST":
+        print('reg')
+        result = create_user(request.form.get('auth_login'),request.form.get('auth_pass'), request.form.get('auth_email'), request.form.get('auth_number'))
+        if result:    
+            return render_template('main.html')   
+    return render_template('register.html', status = result) 
+    
 folder = os.getcwd()
 app = Flask(__name__, template_folder=folder, static_folder=folder)  
 app.add_url_rule('/', 'index', index, methods=['post', 'get'])
 app.add_url_rule('/auth', 'auth', auth, methods=['post', 'get'])
 app.add_url_rule('/catalog', 'catalog', catalog, methods=['post', 'get'])
+app.add_url_rule('/reg', 'reg', reg, methods=['post', 'get'])
 
 if __name__ == "__main__":
     # Запускаем веб-сервер:
--- startSqlite.py
+++ startSqlite.py
@@ -21,14 +21,16 @@ def do(query):
 def create():
     open()
     cursor.execute('''PRAGMA foreign_keys=on''')
-
+    
     do('''CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY not null, 
             login VARCHAR not null,
-            password VARCHAR not null)'''
+            password VARCHAR not null, 
+            email VARCHAR not null,
+            phone INTEGER not null)'''
        )
     
-    do('''INSERT INTO users (login, password) values ('admin', 'admin')''')
+    do('''INSERT INTO users (login, password, email, phone) values ('admin', 'admin', 'admin@corp.ru', '87775553535')''')
 
 def get_succes_auth(login, pass_w):
     open()
@@ -40,10 +42,30 @@ def get_succes_auth(login, pass_w):
         return str(result[1]) == login and str(result[2]) == pass_w
     else:
         return False
+def check_login_email(login, email):
+    open()
+    cursor.execute('''select * from users where login == (?) or email == (?)''', [login, email])
+    result = cursor.fetchall()
+    close()
+    if result != None and len(result) > 0:
+        return False
+    else:
+        return True
+def create_user(login, pass_w, email, phone):
+    if check_login_email(login, email):
+        open()
+        cursor.execute('''insert into users (login, password, email, phone) values (?, ?, ?, ?)''', [login, pass_w, email, phone])
+        print('succesful')
+        conn.commit()
+        close()
+        return True
+    else:
+        return False
+
 
 def main():
-    pass
-    #create()
+    #pass
+    create()
 
 if __name__ == "__main__":
     main()
